0c3e5beb643390b29de331d506cdd02a21999b24
MahmoudMans <57843834+MahmoudMans@users.noreply.github.com>
2020-07-18 14:59:59 +0100

    Add files via upload

--- person.js
+++ person.js
@@ -1,33 +1,17 @@
-let express = require("express");
-let db = require("./db");
-let weather = require("./weather");
-let axios = require("axios");
-
-db._connect();
-
-let app = express();
-app.use(express.json());
-
-app.get("/:name", (req, res) => {
-  let data = axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${req.params.name}&appid=f02e3e68b5adc1fe82f2898ceb50bf99`
-  );
-  axios.all([data]).then(
-    axios.spread((responce) => {
-      let result = {
-        name: responce.data.name,
-        temp: (responce.data.main.temp - 273, 15),
-        date: Date.now(),
-      };
-      weather.create(result, (err, dat) => {
-        if (err) throw err;
-        res.send(dat);
-      });
-    })
-  );
+let mongoose = require("mongoose");
+let validtor = require("validator");
+let Person = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  favoriteFoods: [
+    {
+      type: String,
+    },
+  ],
 });
-
-let port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log("server is running on port : " + port);
-});
+module.exports = mongoose.model("person", Person);
